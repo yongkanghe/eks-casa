0176adaa98febdbff7e9d2e2361be1ee2c243f37
Yongkang He <he@yongkang.cloud>
2022-12-04 13:35:20 +0800

    Create eks-deploy.sh

---
+++ eks-deploy.sh
@@ -0,0 +1,34 @@
+echo '-------Creating an EKS Cluster only (typically about 15 mins)'
+starttime=$(date +%s)
+. ~/.bashrc
+. ./setenv.sh
+
+echo $MY_CLUSTER-$(date +%s)$RANDOM > casa_eks_clustername
+
+eksctl create cluster \
+  --name $(cat casa_eks_clustername) \
+  --version $MY_K8S_VERSION \
+  --nodegroup-name workers4yong1 \
+  --nodes 1 \
+  --nodes-min 1 \
+  --nodes-max 3 \
+  --node-type $MY_INSTANCE_TYPE \
+  --ssh-public-key ~/.ssh/id_rsa.pub \
+  --region $MY_REGION \
+  --ssh-access \
+  --managed
+
+aws eks update-kubeconfig --name $(cat casa_eks_clustername) --region $MY_REGION
+
+./csi-enable.sh
+
+echo "" | awk '{print $1}'
+./pg-deploy.sh
+
+endtime=$(date +%s)
+duration=$(( $endtime - $starttime ))
+echo "-------Total time to build an EKS cluster is $(($duration / 60)) minutes $(($duration % 60)) seconds."
+echo "" | awk '{print $1}'
+echo "-------Created by Yongkang"
+echo "-------Email me if any suggestions or issues he@yongkang.cloud"
+echo "" | awk '{print $1}'
