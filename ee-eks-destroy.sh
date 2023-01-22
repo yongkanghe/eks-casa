9607163b85863f83208f22ce1b2a66cdf5ec9981
Yongkang He <he@yongkang.cloud>
2023-01-22 20:40:12 +0800

    Update ee-eks-destroy.sh

--- ee-eks-destroy.sh
+++ ee-eks-destroy.sh
@@ -17,7 +17,7 @@ echo "" | awk '{print $1}'
 
 endtime=$(date +%s)
 duration=$(( $endtime - $starttime ))
-echo "-------Total time is $(($duration / 60)) minutes $(($duration % 60)) seconds."
+echo "-------Total time to clean up is $(($duration / 60)) minutes $(($duration % 60)) seconds."
 echo "" | awk '{print $1}'
 echo "-------Created by Yongkang"
 echo "-------Email me if any suggestions or issues he@yongkang.cloud"
