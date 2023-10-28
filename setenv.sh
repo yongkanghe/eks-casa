6f448fe76c78e3c79c28643b79696ae0d2689bfa
Yongkang He <he@yongkang.cloud>
2023-10-28 15:00:36 +0800

    Update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -1,7 +1,8 @@
 #-------Set the environment variables"
 export MY_CLUSTER=eks4yong1         #Customize your cluster name
 export MY_INSTANCE_TYPE=t3a.large   #Customize your favorite machine type
-export MY_REGION=us-east-2     #Customize your favorite region
-export MY_ZONE=us-east-2c      #Customize your favorite zone
+export MY_REGION=us-east-2          #Customize your favorite region
+export MY_ZONE=us-east-2c           #Customize your favorite zone
 export MY_K8S_VERSION=1.28          #Customize your Kubernetes Version
+export MY_BUCKET=bucket4velero1     #Customize your bucket name
 source ~/.bashrc                    #Set environment variables
