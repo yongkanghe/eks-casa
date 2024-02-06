8419cba5fc823e8d802640306b1bb2bcdded9692
Yongkang He <he@yongkang.cloud>
2024-02-07 09:42:53 +1100

    Update KubeArmor

--- karmor-destroy.sh
+++ karmor-destroy.sh
@@ -4,6 +4,7 @@ echo '-------Deleting KubeArmor from the EKS Cluster (typically in few mins)'
 # Uninstall KubeArmor
 helm uninstall kubearmor-operator -n yong-kubearmor
 kubectl delete ns yong-nginx
+kubectl delete ns yong-kubearmor
 
 echo "" | awk '{print $1}'
 endtime=$(date +%s)
